0a0127498f359f63f361301e6b253f07474ea35d
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-07-08 19:06:16 -0300

    Validations and associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :orders
+  # , dependent: :destroy (não vamos querer guardar registro de todas as ordens?)
 end
