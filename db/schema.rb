c3c9bdd2f9c2c3b4f4596ad138b44b2b8681ead2
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-07-08 17:57:16 -0300

    Product model created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_204853) do
+ActiveRecord::Schema.define(version: 2020_07_08_205358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.text "description"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
